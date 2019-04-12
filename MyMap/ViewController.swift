ec2ea6c1a6ebab64a2b849f9cc82407bd50e968b
優樹永井 <wadachi0505@gmail.com>
2019-04-12 23:42:04 +0900

    地図アプリ

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate {
+    
+    @IBOutlet var inputTextField: UITextField!
+    @IBOutlet var displayMap: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        inputTextField.delegate = self
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if let searchKey = textField.text {
+            print(searchKey)
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(searchKey) { (placemarks, error) in
+                if let unwrapPlacemarks = placemarks {
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        if let location = firstPlacemark.location {
+                            let targetCoordinate = location.coordinate
+                            print(targetCoordinate)
+                            let pin = MKPointAnnotation()
+                            pin.coordinate = targetCoordinate
+                            pin.title = searchKey
+                            self.displayMap.addAnnotation(pin)
+                            self.displayMap.region = MKCoordinateRegion(center: targetCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
+    
+    @IBAction func changeMapButton() {
+        if displayMap.mapType == .standard {
+            displayMap.mapType = .satellite
+        } else if displayMap.mapType == .satellite {
+            displayMap.mapType = .hybrid
+        } else if displayMap.mapType == .hybrid {
+            displayMap.mapType = .satellite
+        } else if displayMap.mapType == .satellite {
+            displayMap.mapType = .hybridFlyover
+        } else if displayMap.mapType == .hybridFlyover {
+            displayMap.mapType = .mutedStandard
+        } else {
+            displayMap.mapType = .standard
+        }
+    }
 
 }
 
